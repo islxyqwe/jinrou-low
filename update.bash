000175f7438bfb5adaf3bccfbdf401174207b12d
Sunev <ccg6612@163.com>
2021-11-03 22:10:09 +0800

    fix translation

--- update.bash
+++ update.bash
@@ -2,9 +2,9 @@
 forever stop jinrou
 git reset --hard
 git pull
-npm ci
+# npm ci
 cd front/
-npm ci
+# npm ci
 npm run production-build
 cd ..
 SS_ENV=production SS_PACK=1 forever start -l ../logs/out.log -e ../logs/err.log -a --uid "jinrou" app.js

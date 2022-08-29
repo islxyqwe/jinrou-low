15df96588d2f20a622705435c0b5e9841badb26f
Yukimir <luodadi.huazhu@gmail.com>
2022-08-29 23:02:42 +0800

    update cd

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export version=$(cat package.json | jq .version -r)-custom-$TRAVIS_COMMIT
+export git_hash=$(git rev-parse --short "$GITHUB_SHA")
+export version=$(cat package.json | jq .version -r)-custom-$git_hash
 echo $version
 docker build . --tag yukimir/jinrou:$version
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin

ad1c927958865176c67ee84d1291e3af5780e550
Yukimir <luodadi.huazhu@gmail.com>
2022-08-29 22:54:34 +0800

    edit push script

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export version=$(cat package.json | jq .version -r)-custom-$TRAVIS_COMMIT
+echo $version
 docker build . --tag yukimir/jinrou:$version
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push yukimir/jinrou:$version

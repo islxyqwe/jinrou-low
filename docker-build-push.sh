80f764fa1ea0e4abe48181625112cc4e0be6b8d7
Yukimir <luodadi.huazhu@gmail.com>
2022-08-29 22:26:28 +0800

    add publish script

---
+++ docker-build-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export version=${cat package.json | jq .version}-custom
+docker build . --tag yukimir/jinrou:$version
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push yukimir/jinrou:$version
